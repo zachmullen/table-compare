0d2f756bfce54b2da8985c7d19aec4e336779405
Zach Mullen <zach.mullen@kitware.com>
2014-11-17 21:16:56 -0500

    Add a deviation vis mode with linear interpolation in the lightness (HSL) space.

--- table-compare.js
+++ table-compare.js
@@ -1,6 +1,51 @@
 (function ($) {
     'use strict';
 
+    var rgbToHsl = function (r, g, b) {
+        r /= 255, g /= 255, b /= 255;
+        var max = Math.max(r, g, b), min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+
+        if(max == min){
+            h = s = 0; // achromatic
+        } else {
+            var d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+            switch (max){
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+            h /= 6;
+        }
+
+        return [h, s, l];
+    };
+
+    var hslToRgb = function (h, s, l) {
+        var r, g, b;
+
+        if (s == 0){
+            r = g = b = l; // achromatic
+        } else {
+            var hue2rgb = function (p, q, t) {
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    };
+
     /**
      * Validates the data and baseline matrices. Ensures that they are both
      * rectangular and of the same dimension.
@@ -44,6 +89,9 @@
         if (opts.mode === 'binary') {
             return _getColorValueBinary(value, baseline, opts);
         }
+        if (opts.mode === 'linterp') {
+            return _getColorValueLinearInterp(value, baseline, opts);
+        }
     };
 
     var _getColorValueBinary = function (value, baseline, opts) {
@@ -51,6 +99,26 @@
             return opts.nullColor;
         }
         return value > baseline ? opts.highColor : opts.lowColor;
+    };
+
+    var _getColorValueLinearInterp = function (value, baseline, opts) {
+        if (baseline === null) {
+            return opts.nullColor;
+        }
+        var delta = value - baseline[0];
+        var hsl;
+
+        if (delta > 0) {
+            hsl = rgbToHsl(opts.highColor.r, opts.highColor.g, opts.highColor.b);
+            hsl[2] = 1 - delta / baseline[1];
+        } else {
+            hsl = rgbToHsl(opts.lowColor.r, opts.lowColor.g, opts.lowColor.b);
+            hsl[2] = 1 + delta / baseline[1];
+        }
+        hsl[2] = Math.max(hsl[2], 0);
+        hsl[2] = Math.min(hsl[2], 1);
+        var rgb = hslToRgb(hsl[0], hsl[1], hsl[2]);
+        return {r: rgb[0], g: rgb[1], b: rgb[2]};
     };
 
     /**
@@ -79,13 +147,14 @@
             headers: null,
             styleAttribute: 'backgroundColor',
             mode: 'binary',
-            highColor: '#c75',
-            lowColor: '#7ad',
-            nullColor: '#fff'
+            highColor: {r: 204, g: 119, b: 119},
+            lowColor: {r: 119, g: 170, b: 119},
+            nullColor: {r: 255, g: 255, b: 255},
+            dynamicTextColor: false
         };
         $.extend(opts, config || {});
 
-        if (['binary'].indexOf(opts.mode) < 0) {
+        if (['binary', 'linterp'].indexOf(opts.mode) < 0) {
             console.error('Invalid compareTable mode: ' + opts.mode);
             return;
         }
@@ -109,7 +178,14 @@
 
             $.each(row, function (j, val) {
                 var cell = $('<td row-number="' + i + '" col-number="' + j + '">');
-                cell.css(opts.styleAttribute, _getColorValue(val, baseline[i][j], opts));
+                var rgb = _getColorValue(val, baseline[i][j], opts);
+
+                cell.css(opts.styleAttribute, 'rgb(' + rgb.r + ', ' + rgb.g + ', ' + rgb.b + ')');
+
+                if (opts.dynamicTextColor) {
+                    var hsl = rgbToHsl(rgb.r, rgb.g, rgb.b);
+                    cell.css('color', hsl[2] > 0.5 ? 'black' : 'white');
+                }
                 cell.text(val);
                 cell.appendTo(rowEl);
             });
